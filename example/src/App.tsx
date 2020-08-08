bcd64c81024c92c934d1d468cb0f92e64d30bb22
vinit_groww <vinitraj1012@gmail.com>
2020-08-08 15:55:31 +0530

    chore: added jetpack camerax

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    Camerax.multiply(3, 7).then(setResult);
+    Camerax.multiply(3, 9).then(setResult);
   }, []);
 
   return (
